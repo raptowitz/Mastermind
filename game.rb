1c5625aaa9f0beb46c6897ca5a5f159d0284f09f
Rachael Aptowitz <raptowitz@gmail.com>
2021-01-13 19:47:09 -0800

    Computer player class

--- game.rb
+++ game.rb
@@ -2,21 +2,32 @@
 
 # Terminal output colors
 module Colors
-  def colorize(code)
-    @code = code
-    "\e[#{code}m#{self}\e[0m"
+  def red(text)
+    "\e[41m#{text}\e[0m"
   end
 
-  def red
-    colorize(31)
+  def green(text)
+    "\e[42m#{text}\e[0m"
   end
 
-  def green
-    colorize(32)
+  def yellow(text)
+    "\e[43m#{text}\e[0m"
   end
 
-  def hello
-    puts 'hello'.red
+  def blue(text)
+    "\e[44m#{text}\e[0m"
+  end
+
+  def magenta(text)
+    "\e[45m#{text}\e[0m"
+  end
+
+  def cyan(text)
+    "\e[46m#{text}\e[0m"
+  end
+
+  def gray(text)
+    "\e[47m#{text}\e[0m"
   end
 end
 
@@ -25,7 +36,7 @@ class Board
   include Colors
 
   def initialize
-    @board = Array.new(12) { Array.new(4, '?') }
+    @board = Array.new(12) { Array.new(4, gray('?')) }
   end
 
   def print_board
@@ -35,8 +46,19 @@ class Board
   end
 end
 
+# Random computer player
 class Computer
-  
+  def initialize
+    @choices = ['r', 'g', 'y', 'b', 'm', 'c', '']
+    @secretcode = []
+  end
+
+  def select_colors
+    4.times do
+      @selection = rand(7)
+      @secretcode.push(@choices[@selection])
+    end
+  end
 end
 
 
